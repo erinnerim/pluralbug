5d163e7751d48c5c1657e4d478a1c8ceb52f2ec6
erinnerim <erinnerim@gmail.com>
2023-08-12 14:57:30 -0700

    Rename setup.py from generic template

--- setup.py
+++ setup.py
@@ -20,15 +20,15 @@ def read_file(path_segments):
         return f.read()
 
 
-version = exec_file(("my_project_name", "__init__.py"))["__version__"]
+version = exec_file(("pluralbug", "__init__.py"))["__version__"]
 long_description = read_file(("README.md",))
 
 
 setup(
-    name="my-project-name",
+    name="pluralbug",
     version=version,
-    url="https://github.com/anoadragon453/nio-template",
-    description="A matrix bot to do amazing things!",
+    url="https://github.com/erinnerim/pluralbug",
+    description="A matrix bot for plural systems.",
     packages=find_packages(exclude=["tests", "tests.*"]),
     install_requires=[
         "matrix-nio[e2e]>=0.10.0",
@@ -55,5 +55,5 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     # Allow the user to run the bot with `my-project-name ...`
-    scripts=["my-project-name"],
+    scripts=["pluralbug"],
 )
